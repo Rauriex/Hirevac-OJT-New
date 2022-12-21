112c6971308e3b78153cebb847952cfac59c1903
Patto <rariexph@gmail.com>
2022-12-22 03:56:30 +0800

    removed clearcache
    attempt

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/clearcache/', include('clearcache.urls')),
     path('admin/', admin.site.urls),
     path('', include('jobsapp.urls')),
     path('', include('accounts.urls')),
 ]
+# Added for vercel
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
