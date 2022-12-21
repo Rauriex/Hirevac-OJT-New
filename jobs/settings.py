4af21b9bbc3bc16ffb11ca06ba14419ff01f8b19
Patto <rariexph@gmail.com>
2022-12-22 02:38:17 +0800

    Update settings.py

--- jobs/settings.py
+++ jobs/settings.py
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'jobs.wsgi.application'
 # Commented out database for Vercel:
 # source: https://www.devmaesters.com/blog/15 (step 5)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Password validation
